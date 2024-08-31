4407fe011dd5a3a32cf1572b2b37fafd0ec60a25
Aman <amanqkhs0@gmail.com>
2024-08-31 21:58:27 +0530

    Added Another file for making Goal Achived.

--- Detect_Same_Images_in_Bigger_Image/DetectSameImages.py
+++ Detect_Same_Images_in_Bigger_Image/DetectSameImages.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-img_rgb = cv2.imread('../Detect_Same_Images_in_Bigger_Image/mario.png')
-template = cv2.imread('../Detect_Same_Images_in_Bigger_Image/mario_coin.png')
+img_rgb = cv2.imread('mario.png')
+template = cv2.imread('mario_coin.png')
 w, h = template.shape[:-1]
 
 res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
-threshold = .9
+threshold = .8
 loc = np.where(res >= threshold)
 for pt in zip(*loc[::-1]):  # Switch columns and rows
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
